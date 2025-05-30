32aca86cef5cb4811d672802e0a596f17fe8530c
zarious-dev <enriquez.caezarie@gmail.com>
2025-05-31 06:51:39 +0800

    enriquez changes

--- resources/js/Pages/CategoryBlogs.jsx
+++ resources/js/Pages/CategoryBlogs.jsx
@@ -5,38 +5,47 @@ import { BackgroundImage } from '@mantine/core';
 
 function CategoryBlogs({ blogs, category }) {
   return (
-    <div className=" bg-gray-100">
+    <div>
       <Navbar />
 
-      {/* Banner */}
-      <div className="relative w-full h-[600px]">
-      {/* Banner Image */}
-      <BackgroundImage
-        src={`http://localhost:8000/storage/${category.thumbnail}`}
-        radius="sm"
-        className="w-full h-full object-cover"
-      >
-        {/* Gradient Overlay and Title */}
-        <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/70 to-transparent px-6 py-4">
-          <h1 className="text-4xl font-bold text-white">{category.categoryName}</h1>
-        </div>
-      </BackgroundImage>
-    </div>
-     
+      <h1 className="text-9xl font-bold text-start text-gray-900 px-25 mt-6 mb-20 ml-20">
+        {category.categoryName}
+      </h1>
 
-      {/* Blog Cards */}
-      <div className="max-w-6xl mx-auto py-10 px-4 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-        {blogs.map((blog) => (
-          <BlogCard
-            key={blog.id}
-            Title={blog.BlogTitle}
-            Description={blog.BlogDescription}
-            Creator={blog.creator}
-            Thumbnail={blog.Thumbnail}
-            id={blog.id}
-            likes={blog.likes}
-          />
-        ))}
+
+      <div className="max-w-full mx-auto ml-15 mr-15">
+        {/* Banner */}
+
+        <div className="h-[500px] px-24">
+          {/* Banner Image */}
+          <BackgroundImage
+            src={`http://localhost:8000/storage/${category.thumbnail}`}
+            radius="sm"
+            className="w-full h-full object-cover"
+          >
+            {/* Gradient Overlay and Title */}
+            {/* <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/70 to-transparent px-6 py-4">
+          <h1 className="text-4xl font-bold text-white">{category.categoryName}</h1>
+        </div> */}
+          </BackgroundImage>
+        </div>
+
+
+        {/* Blog Cards */}
+        {/* <div className="max-w-6xl mx-auto py-10 px-4 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 bg-pink-100"> */}
+        <div className="max-w-full mx-auto my-auto p-10 grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 bg-pink-100 mt-16 ml-25 mr-25">
+          {blogs.map((blog) => (
+            <BlogCard
+              key={blog.id}
+              Title={blog.BlogTitle}
+              Description={blog.BlogDescription}
+              Creator={blog.creator}
+              Thumbnail={blog.Thumbnail}
+              id={blog.id}
+              likes={blog.likes}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- resources/js/components/CategoriesCard.jsx
+++ resources/js/components/CategoriesCard.jsx
@@ -5,12 +5,17 @@ import DreamLog from '../assets/DreamLog.png';
 function CategoriesCard({Category, Thumbnail}) {
   return (
     <div
-      className="relative w-[250px] h-[250px] bg-cover bg-center bg-slate-200 rounded-lg"
+      className="relative w-[300px] h-[300px] bg-cover bg-center bg-slate-200 rounded-3xl transition-transform duration-300 hover:scale-110"
       style={{ backgroundImage: `url("http://localhost:8000/storage/${Thumbnail}")` }}
     >
-    <h1 className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-2xl font-bold text-white z-10">
+    <h1 className="absolute bottom-4 left-0 w-full text-xl text-center font-bold text-white z-10 px-2 py-1"
+    style={{backgroundColor: "#00000040"}} >
         {Category}
     </h1>
+
+    {/* <h1 className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-xl font-bold text-white z-10 ">
+        {Category}
+    </h1> */}
     </div>
   )
 }
