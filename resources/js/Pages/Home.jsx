f4e993808339a03931a51a4c215bf04f9d1c3fef
zarious-dev <enriquez.caezarie@gmail.com>
2025-06-03 07:53:52 +0800

    enriquez update

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -21,7 +21,7 @@ function Card({ image, title, category, id }) {
           <Text className="category" size="xs">
             {category}
           </Text>
-          <Title order={3} className={"title"}>
+          <Title order={1} className={"title"}>
             {title}
           </Title>
         </div>
@@ -36,103 +36,106 @@ function Card({ image, title, category, id }) {
 }
 
 
-function Home( {blogs}) {
+function Home({ blogs }) {
 
   const { categories } = usePage().props;
-    
+
   return (
     <>
-        <Navbar  categories={categories} />
-        
-        <div className="flex flex-col gap-20 w-full ">
-          <section className='flex flex-col justify-center items-center w-full gap-5'>
-            <Title order={2} classname= 'm-5 p-5'>Top Blogs</Title> 
-            <Carousel withIndicators className="h-[400px] w-[1500px]">
-              
-              {blogs
-                .sort((a, b) => b.likes - a.likes) // Sort blogs by likes in descending order
-                .slice(0, 5) // Take top 5 blogs
-                .map((blog) => (
-                  
-                  <Carousel.Slide>
-                    <Card
-                      image={`http://localhost:8000/storage/${blog.Thumbnail}`}
-                      title={blog.BlogTitle}
-                      category={blog.BlogCategory}
-                      id={blog.id}
-                    />
-                  </Carousel.Slide>
-                 
-              ))}
+      <Navbar categories={categories} />
 
-             
-              {/* ...other slides */}
-            </Carousel>
-          </section>
+      <div className="flex flex-col gap-20 w-full ">
+        <section className='flex flex-col justify-center items-center w-full gap-5'>
+          {/* <Title order={2} classname= 'm-5 p-5'>Top Blogs</Title> */}
 
-          <section className='w-full  bg-pink-50   rounded-xl pb-10'>
-            <Title order={2} className=' p-10'>Most Viewed</Title>
-            <div className='flex flex-row justify-center items-center gap-6'>
-                {blogs
-                  .sort((a, b) => b.view_count - a.view_count)  // Sort in descending order of views
-                  .slice(0, 5)                                   // Take top 5
-                  .map((blog) => (
-                    <BlogCard
-                      Title={blog.BlogTitle}
-                      Description={blog.BlogDescription}
-                      Creator={blog.creator}
-                      Thumbnail={blog.Thumbnail}
-                      id={blog.id}
-                      likes={blog.likes}
-                    />
-                ))}
-              </div>
-            
-          </section>
+          <h1 className="text-5xl font-extrabold text-gray-900 dark:text-black p-5">TOP BLOGS
+          </h1>
+          <Carousel withIndicators className="h-[400px] w-[1500px]">
 
-          <section className='w-full  rounded-xl pb-10'>
-           <Title order={2} className=' p-10'>Discover more</Title>
-            <div className='flex flex-row justify-center items-center gap-6'>
-              {blogs.map((blog)=>(
-                                <BlogCard
-                                  Title={blog.BlogTitle}
-                                  Description={blog.BlogDescription}
-                                  Creator={blog.creator}
-                                  Thumbnail={blog.Thumbnail}
-                                  id={blog.id}
-                                  
-                                  likes={blog.likes}/>
-                            ))}
-            </div>
-            
-          </section>
+            {blogs
+              .sort((a, b) => b.likes - a.likes) // Sort blogs by likes in descending order
+              .slice(0, 5) // Take top 5 blogs
+              .map((blog) => (
 
-          <section className='w-full flex flex-col items-start bg-pink-50 rounded-xl pt-10'>
-  
-            <Title order={2} className="p-10">
-              Multiple Categories to Choose From
-            </Title>
-
-            <div className="flex flex-row flex-wrap gap-3 m-10">
-              {categories.map((category) => (
-                <a key={category.id} href={`category/${category.id}`} className="m-3">
-                  <CategoriesCard
-                    Category={category.categoryName}
-                    Thumbnail={category.thumbnail}
+                <Carousel.Slide>
+                  <Card
+                    image={`http://localhost:8000/storage/${blog.Thumbnail}`}
+                    title={blog.BlogTitle}
+                    category={blog.BlogCategory}
+                    id={blog.id}
                   />
-                </a>
-              ))}
-            </div>
-          
-          </section>
+                </Carousel.Slide>
 
-        </div>
-        
-        <footer className='h-[100px] mt-[100px] flex flex-col justify-center items-center bg-slate-200'>
-          <span>2025 Dream Log. All Rights Reserved.</span>
-        </footer>
+              ))}
+
+
+            {/* ...other slides */}
+          </Carousel>
+        </section>
+
+        <section className='w-full  bg-pink-50   rounded-xl pb-10'>
+          <Title order={2} className=' p-10'>MOST VIEWED BLOGS</Title>
+          <div className='flex flex-row justify-center items-center gap-6'>
+            {blogs
+              .sort((a, b) => b.view_count - a.view_count)  // Sort in descending order of views
+              .slice(0, 5)                                   // Take top 5
+              .map((blog) => (
+                <BlogCard
+                  Title={blog.BlogTitle}
+                  Description={blog.BlogDescription}
+                  Creator={blog.creator}
+                  Thumbnail={blog.Thumbnail}
+                  id={blog.id}
+                  likes={blog.likes}
+                />
+              ))}
+          </div>
+
+        </section>
+
+        <section className='w-full  rounded-xl pb-10'>
+          <Title order={2} className=' p-10'>DISCOVER MORE</Title>
+          <div className='flex flex-row justify-center items-center gap-6'>
+            {blogs.map((blog) => (
+              <BlogCard
+                Title={blog.BlogTitle}
+                Description={blog.BlogDescription}
+                Creator={blog.creator}
+                Thumbnail={blog.Thumbnail}
+                id={blog.id}
+
+                likes={blog.likes} />
+            ))}
+          </div>
+
+        </section>
+
+        <section className='w-full flex flex-col items-start bg-pink-50 rounded-xl pt-10'>
+
+          <Title order={2} className="p-10">
+            MULTIPLE CATEGORIES TO CHOOSE
+          </Title>
+
+          <div className="flex flex-row flex-wrap gap-3 m-10">
+            {categories.map((category) => (
+              <a key={category.id} href={`category/${category.id}`} className="m-3">
+                <CategoriesCard
+                  Category={category.categoryName}
+                  Thumbnail={category.thumbnail}
+                />
+              </a>
+            ))}
+          </div>
+
+        </section>
+
+      </div>
+
+      <footer className='h-[100px] mt-[100px] flex flex-col justify-center items-center bg-slate-200'>
+        <span>2025 Dream Log. All Rights Reserved.</span>
+      </footer>
     </>
-    
+
   )
 }
 
