7d9b9b707868c24aef530c669dbe10a7c0b35171
Ceejay Santos <7078ceejay@gmail.com>
2025-06-03 21:24:00 +0800

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  base: process.env.ASSET_URL ? process.env.ASSET_URL + '/' : '/',
-
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.jsx'],
-      refresh: true,
-    }),
-    react(),
-  ],
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'resources/js'),
+    
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            refresh: true,
+        }),
+        react(),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
     },
-  },
+   
 });
+
+
